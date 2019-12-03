49eda49652d0af29ccb60ebfeb2094488c04eb0d
Renato Bastos <renato.bastos@hotmail.com>
2019-12-03 00:34:42 -0200

    Casa 12/02 - 2

--- noah/src/br/com/noah/controller/mainController.java
+++ noah/src/br/com/noah/controller/mainController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class OlaMundoController {
-	@RequestMapping("/olaMundoSpring")
+public class mainController {
+	@RequestMapping("/main")
 	public String execute() {
 		System.out.println("Executando um seviço com o Spring MVC");
 		return "login";
